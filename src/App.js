2f682cdf6a2c236a3670f1f4d3755661370e6e02
Leonardoasd <126222979+Leonardoasd@users.noreply.github.com>
2024-01-03 06:55:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { React , useState } from "react";
+import ReactMarkdown from "react-markdown"
 
 function App() {
+
+  const initialMarkdown = `# ¡Bienvenido a mi Proyecto!
+
+  ## Descripción
+  Este proyecto es un lugar donde exploraré diferentes funcionalidades y técnicas utilizando Markdown para crear contenido dinámico y atractivo.
+  
+  ## Objetivos
+  - Aprender a utilizar Markdown para dar formato a texto.
+  - Practicar la creación de listas, tablas y otros elementos.
+  - Integrar Markdown en aplicaciones web.
+  
+  ¡Espero que disfrutes explorando este proyecto tanto como yo disfruto creándolo!
+  `;
+
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='editorContainer'>
+        <div className='miniNavbar' >
+          <h3>Editor</h3>
+        </div>
+        <textarea id="editor" className='editor' value={markdown} onChange={(e) => setMarkdown(e.target.value)}  />
+      </div>
+      <div className='markContainer' >
+        <div>
+          <h3 className='miniNavbar'>Previewer</h3>
+        </div>
+        <ReactMarkdown id="preview" className="markdown">{markdown}</ReactMarkdown>
+      </div>
     </div>
   );
 }
